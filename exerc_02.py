ca8e1a9635407f1551223187254035844a09be75
daniel.ribas <daniel.ribas@isdbflowtech.com>
2025-09-25 13:43:49 -0300

    update project

--- exerc_02.py
+++
@@ -1,8 +0,0 @@
-
-def calcular_idade(ano_nascimento, ano_atual):
-    return ano_atual - ano_nascimento
-
-ano_nascimento = input('Digite o ano de nascimento: ')
-ano_atual = input('Digite o ano atual: ')
-
-print(f'A idade é {calcular_idade(int(ano_nascimento), int(ano_atual))} anos.')
